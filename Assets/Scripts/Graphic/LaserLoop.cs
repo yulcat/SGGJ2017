4c089fb6d187a0753d449476ac166085869b4ff1
yulcat <alszmtm@gmail.com>
2017-11-04 20:18:54 +0900

    레이저 프리팹 추가

---
+++ Assets/Scripts/Graphic/LaserLoop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LaserLoop : MonoBehaviour
+{
+    public float LengthMultiplier = 1;
+    public float TimeMultiplier = 1;
+    LineRenderer rend;
+    void Start()
+    {
+        rend = GetComponent<LineRenderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rend.material.SetFloat("_Length", rend.GetPosition(1).y * LengthMultiplier);
+        rend.material.SetFloat("_Offset", Time.time * TimeMultiplier);
+    }
+}
