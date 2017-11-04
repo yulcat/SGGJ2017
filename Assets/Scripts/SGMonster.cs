d6f85ff5b6f63f6a7a27c916d5e491b6865cf170
yulcat <alszmtm@gmail.com>
2017-11-04 22:47:17 +0900

    몬스터별 ai 작성할 수 있도록 구조 분리

--- Assets/Scripts/SGMonster.cs
+++ Assets/Scripts/SGMonster.cs
@@ -16,36 +16,36 @@ public class SGMonster : SGCharacter
         ATTACK_TO_HERO,
         ATTACK_TO_BASE
     }
+    public enum MONSTER_TYPE
+    {
+        ZOMBIE,
+        WHEEL,
+        BIG
+    }
 
-    SGE_MONSTER_ACTION_STATE actionState = SGE_MONSTER_ACTION_STATE.TRACE_DESTINATION;
-    public SGE_MONSTER_ACTION_STATE GetActionState { get { return actionState; } }
+    public MONSTER_TYPE monsterType = MONSTER_TYPE.ZOMBIE;
 
     public float sightLength;
 
     float distanceToHero = Mathf.Infinity;   //영웅과의 거리
     Animator myAnimator;
     Rigidbody2D body;
+    IMonsterPattern pattern;
 
     // Use this for initialization
     override protected void Start()
     {
+        switch (monsterType)
+        {
+            case MONSTER_TYPE.ZOMBIE: pattern = new NormalZombie(this); break;
+            case MONSTER_TYPE.WHEEL: pattern = new WheelZombie(this); break;
+        }
         myAnimator = gameObject.GetComponentInChildren<Animator>();
         body = GetComponent<Rigidbody2D>();
         gameObject.UpdateAsObservable().Subscribe(_ =>
         {
             float distanceToHero = Vector3.Distance(transform.position, SGGameManager.Instance.hero.transform.position);
-
-            if (distanceToHero <= sightLength)
-            {
-                actionState = SGE_MONSTER_ACTION_STATE.TRACE_HERO;
-                if (distanceToHero <= 1f)
-                {
-                    actionState = SGE_MONSTER_ACTION_STATE.ATTACK_TO_HERO;
-                }
-            }
-            else if (actionState != SGE_MONSTER_ACTION_STATE.ATTACK_TO_BASE)
-                actionState = SGE_MONSTER_ACTION_STATE.TRACE_DESTINATION;
-
+            pattern.SetActionState(distanceToHero);
             if (GetAliveState == SGE_ALIVE_STATE.DEAD)
             {
                 Destroy(gameObject);
@@ -55,38 +55,21 @@ public class SGMonster : SGCharacter
         gameObject.FixedUpdateAsObservable().Subscribe(_ =>
         {
             if (!movable) return;
-            if (actionState == SGE_MONSTER_ACTION_STATE.TRACE_DESTINATION || actionState == SGE_MONSTER_ACTION_STATE.TRACE_HERO)
-            {
-                Vector3 destination = Vector3.zero;
-
-                if (actionState == SGE_MONSTER_ACTION_STATE.TRACE_DESTINATION)
-                    destination = SGGameManager.Instance.CurrentMonsterDestination.position;
-                else if (actionState == SGE_MONSTER_ACTION_STATE.TRACE_HERO)
-                    destination = SGGameManager.Instance.hero.transform.position;
-
-                RotateToLookup((transform.position - destination).normalized);
-
-                body.velocity = (destination - transform.position).normalized * currentMoveSpeed;
-            }
-            else
-            {
-                myAnimator.SetTrigger("Attack");
-            }
-
+            pattern.PerformAction();
         });
 
         base.Start();
     }
 
-    void RotateToLookup(Vector3 target)
+    void RotateToLookup(Vector3 target, float t = 1)
     {
         Vector3 myPos = transform.up;
-        transform.up = Vector3.Slerp(myPos, target, 1f);
+        transform.up = Vector3.Slerp(myPos, target, t);
     }
 
     public void AttackToBase()
     {
-        actionState = SGE_MONSTER_ACTION_STATE.ATTACK_TO_BASE;
+        pattern.AttackBase();
     }
 
     public override bool AnyDamage(float damage, System.Guid guid)
@@ -96,6 +79,89 @@ public class SGMonster : SGCharacter
         return true;
     }
 
+    interface IMonsterPattern { void SetActionState(float distance); void PerformAction(); void AttackBase(); }
+    class NormalZombie : IMonsterPattern
+    {
+        SGE_MONSTER_ACTION_STATE actionState = SGE_MONSTER_ACTION_STATE.TRACE_DESTINATION;
+        SGMonster me;
 
+        public NormalZombie(SGMonster parent)
+        {
+            me = parent;
+        }
 
+        public void SetActionState(float distanceToHero)
+        {
+            if (distanceToHero <= me.sightLength)
+            {
+                actionState = SGE_MONSTER_ACTION_STATE.TRACE_HERO;
+                if (distanceToHero <= 1f)
+                {
+                    actionState = SGE_MONSTER_ACTION_STATE.ATTACK_TO_HERO;
+                }
+            }
+            else if (actionState != SGE_MONSTER_ACTION_STATE.ATTACK_TO_BASE)
+                actionState = SGE_MONSTER_ACTION_STATE.TRACE_DESTINATION;
+        }
+
+        public void PerformAction()
+        {
+            if (actionState == SGE_MONSTER_ACTION_STATE.TRACE_DESTINATION || actionState == SGE_MONSTER_ACTION_STATE.TRACE_HERO)
+            {
+                Vector3 destination = Vector3.zero;
+
+                if (actionState == SGE_MONSTER_ACTION_STATE.TRACE_DESTINATION)
+                    destination = SGGameManager.Instance.CurrentMonsterDestination.position;
+                else if (actionState == SGE_MONSTER_ACTION_STATE.TRACE_HERO)
+                    destination = SGGameManager.Instance.hero.transform.position;
+
+                me.RotateToLookup((me.transform.position - destination).normalized);
+
+                me.body.velocity = (destination - me.transform.position).normalized * me.currentMoveSpeed;
+            }
+            else
+            {
+                me.myAnimator.SetTrigger("Attack");
+            }
+        }
+
+        public void AttackBase()
+        {
+            actionState = SGE_MONSTER_ACTION_STATE.ATTACK_TO_BASE;
+        }
+    }
+
+    class WheelZombie : IMonsterPattern
+    {
+        bool tracingDestination;
+        SGMonster me;
+        const float maxSpeed = 6;
+        const float rotationSpeed = 0.3f;
+        public WheelZombie(SGMonster parent)
+        {
+            me = parent;
+        }
+
+        public void AttackBase()
+        {
+            tracingDestination = true;
+        }
+
+        public void PerformAction()
+        {
+            Vector3 destination;
+            if (tracingDestination)
+                destination = SGGameManager.Instance.CurrentMonsterDestination.position;
+            else
+                destination = SGGameManager.Instance.hero.transform.position;
+            me.RotateToLookup((me.transform.position - destination).normalized, rotationSpeed);
+            var acc = -me.transform.up * me.moveSpeed * Time.deltaTime;
+            me.body.velocity = (me.body.velocity + (Vector2)acc) * (1 - Time.deltaTime * (1 - me.moveSpeed / maxSpeed));
+        }
+
+        public void SetActionState(float distanceToHero)
+        {
+            tracingDestination = distanceToHero > me.sightLength;
+        }
+    }
 }
