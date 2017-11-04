ebfb702884a13e0ac101a436d4f3660134502493
WSavory <nekoyang2@gmail.com>
2017-11-05 04:50:30 +0900

    stage2수정

--- Assets/Scripts/SGGameStart.cs
+++ Assets/Scripts/SGGameStart.cs
@@ -61,23 +61,15 @@ public class SGGameStart : SGSingleton<SGGameStart>
         {
             case 1:
                 TitleText.text = "제 1장 : 시작되는 사랑";
-                DescriptionText.text = "휴가를 맞아 던전 여행을 하던...\n\n\n\n몬스터들을 함정으로 유인하여 해치우자";
+                DescriptionText.text = "휴가를 맞아 던전 여행을 하던 커플\n\n여행지에서 사랑을 키우다가 그만 남자친구가 다치고 말았다\n\n여자친구가 나서서 남자친구를 지켜야하는 상황!\n\n몬스터들을 함정으로 유인하여 해치우자";
                 break;
             case 2:
                 TitleText.text = "제 2장 : 피어오르는 사랑";
-                DescriptionText.text = "새로운 몬스터 등장!\n대사는 좀 나중에 생각해봅시다";
+                DescriptionText.text = "위급한 상황에서 사랑은 피어나고..\n\n새로운 몬스터 등장!\n\n이번 몬스터는 좀 더 강합니다\n\n함정으로 유인하여 해치워 봅시다";
                 break;
             case 3:
-                TitleText.text = "제 3장 : 계속되는 사랑";
-                DescriptionText.text = "아..힘들어\n대사는 좀 나중에 생각해봅시다";
-                break;
-            case 4:
-                TitleText.text = "제 4장 : 지쳐가는 사랑";
-                DescriptionText.text = "새로운 몬스터 등장!\n대사는 좀 나중에 생각해봅시다";
-                break;
-            case 5:
                 TitleText.text = "마지막 장 : 영원한 사랑";
-                DescriptionText.text = "그 둘의 사랑은 영원할 것인가?\n대사는 좀 나중에 생각해봅시다";
+                DescriptionText.text = "아..힘들어\n대사는 좀 나중에 생각해봅시다";
                 break;
         }
 
@@ -90,21 +82,27 @@ public class SGGameStart : SGSingleton<SGGameStart>
 
         Counttextobj.SetActive(true);
         counttext.text = "3";
+        SGSoundManager.Instance.PlayButtonSound();
         yield return new WaitForSeconds(1f);
-        LeanTween.scale(Counttextobj.GetComponent<RectTransform>(), Counttextobj.GetComponent<RectTransform>().localScale * 0.01f, 1f);
+        
+        LeanTween.scale(Counttextobj.GetComponent<RectTransform>(), Counttextobj.GetComponent<RectTransform>().localScale * 0.01f, 0.5f);
         yield return new WaitForSeconds(1f);
         counttext.text = "2";
-        LeanTween.scale(Counttextobj.GetComponent<RectTransform>(), Counttextobj.GetComponent<RectTransform>().localScale * 100f, 1f);
+        SGSoundManager.Instance.PlayButtonSound();
+        LeanTween.scale(Counttextobj.GetComponent<RectTransform>(), Counttextobj.GetComponent<RectTransform>().localScale * 100f, 0.5f);
         yield return new WaitForSeconds(1.2f);
-        LeanTween.scale(Counttextobj.GetComponent<RectTransform>(), Counttextobj.GetComponent<RectTransform>().localScale * 0.01f, 1f);
+    
+        LeanTween.scale(Counttextobj.GetComponent<RectTransform>(), Counttextobj.GetComponent<RectTransform>().localScale * 0.01f, 0.5f);
         yield return new WaitForSeconds(1f);
         counttext.text = "1";
-        LeanTween.scale(Counttextobj.GetComponent<RectTransform>(), Counttextobj.GetComponent<RectTransform>().localScale * 100f, 1f);
+        SGSoundManager.Instance.PlayButtonSound();
+        LeanTween.scale(Counttextobj.GetComponent<RectTransform>(), Counttextobj.GetComponent<RectTransform>().localScale * 100f, 0.5f);
         yield return new WaitForSeconds(1.2f);
-        LeanTween.scale(Counttextobj.GetComponent<RectTransform>(), Counttextobj.GetComponent<RectTransform>().localScale * 0.01f, 1f);
+
+        LeanTween.scale(Counttextobj.GetComponent<RectTransform>(), Counttextobj.GetComponent<RectTransform>().localScale * 0.01f, 0.5f);
         yield return new WaitForSeconds(1f);
         counttext.text = "GO!";
-        LeanTween.scale(Counttextobj.GetComponent<RectTransform>(), Counttextobj.GetComponent<RectTransform>().localScale * 100f, 1f);
+        LeanTween.scale(Counttextobj.GetComponent<RectTransform>(), Counttextobj.GetComponent<RectTransform>().localScale * 100f, 0.5f);
         yield return new WaitForSeconds(1.5f);
         LeanTween.scale(Counttextobj.GetComponent<RectTransform>(), Counttextobj.GetComponent<RectTransform>().localScale * 0, 0.3f);
 
