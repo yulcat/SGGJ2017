5a324b89776e71a545ffad92ce7dde5355928065
DESKTOP-45BGSFN\bbang <bbangwon.chung@gmail.com>
2017-11-04 10:14:03 +0900

    stagedata json 추가

--- Assets/Scripts/SGGameManager.cs
+++ Assets/Scripts/SGGameManager.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 using Unity.Linq;
+using LitJson;
 
 public class SGGameManager : SGSingleton<SGGameManager> {
 
     public SGHero hero;
     public Transform stagesTransform;
+    public Transform monstersTransform;
+
+    public enum SGE_GameState
+    {
+        GAME_INIT,  //게임 초기화
+        STAGE_START,
+        GAME_FAIL,
+        GAME_CLEAR
+    }
+
+    ReactiveProperty<SGE_GameState> gameState = new ReactiveProperty<SGE_GameState>(SGE_GameState.GAME_INIT);
+
 
     IntReactiveProperty currentStageNum = new IntReactiveProperty(1);    //현재 스테이지 번호
 
@@ -18,12 +31,68 @@ public class SGGameManager : SGSingleton<SGGameManager> {
     public Transform CurrentMonsterStartPoint { get { return currentStage.gameObject.Child("StartPoint").transform; } }
     public Transform CurrentMonsterDestination { get { return currentStage.gameObject.Child("Destination").transform; } }
 
+    public TextAsset stageJsonAsset;
+    JsonData stageJson;
+
+
+
     // Use this for initialization
     void Start () {
-        allStageCount = stagesTransform.childCount;
 
+        //게임스테이트에 따라
+        gameState.Subscribe(_ =>
+        {
+            switch (_)
+            {
+                case SGE_GameState.GAME_INIT:
+                    GameInit();
+                    break;
+                case SGE_GameState.STAGE_START:
+                    StageStart();
+                    break;
+                case SGE_GameState.GAME_CLEAR:
+                    GameClear();
+                    break;
+                case SGE_GameState.GAME_FAIL:
+                    GameFail();
+                    break;
+            }
+        });
+
+        //스테이지가 변경되면
         currentStageNum.Subscribe(_ => {
             currentStage = stagesTransform.gameObject.Child("Stage" + _).transform; //스테이지 정보를 저장
+            stageJson = SGUtils.GetJsonArrayForKey(JsonMapper.ToObject(stageJsonAsset.text), "stage", _);
+            gameState.Value = SGE_GameState.STAGE_START;
         });
     }
+
+    void GameInit()
+    {
+        allStageCount = stagesTransform.childCount;
+    }
+
+    void StageStart()
+    {
+
+    }
+
+    void StageClear()
+    {
+        if (currentStageNum.Value > allStageCount)
+            gameState.Value = SGE_GameState.GAME_CLEAR;
+        else
+            currentStageNum.Value += 1;
+    }
+
+    void GameClear()
+    {
+
+    }
+
+    void GameFail()
+    {
+
+    }
+    
 }
---
+++ Assets/Scripts/SGUtils.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+public class SGUtils {
+
+    //JSON Array의 값이 JSON Object 일때 키와 값이 맞는 JSON Object를 리턴
+    static public LitJson.JsonData GetJsonArrayForKey(LitJson.JsonData jsonData, string key, object value)
+    {
+        if (jsonData.IsArray == false)
+            return null;
+
+        for (int jsonCnt = 0; jsonCnt < jsonData.Count; jsonCnt++)
+        {
+            if (jsonData[jsonCnt].Keys.Contains(key))
+            {
+                if (value.GetType() == typeof(int))
+                {
+                    if ((int)jsonData[jsonCnt][key] == (int)value)
+                        return jsonData[jsonCnt];
+                }
+                else
+                {
+                    if (jsonData[jsonCnt][key].ToString() == (string)value)
+                        return jsonData[jsonCnt];
+                }
+            }
+        }
+        return null;
+    }
+}
