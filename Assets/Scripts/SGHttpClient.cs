ff53974c4c2e2261e1613fd94c374a26ce7384d9
kdhee09 <kdhee09@gmail.com>
2017-11-04 21:08:02 +0900

    로그인 씬 Http 통신 클래스 추가.

---
+++ Assets/Scripts/SGHttpClient.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Text;
+
+public class SGHttpClient : SGSingleton<SGHttpClient>
+{
+    public const long SUCCESS = 200;
+    public const long NOT_FOUND = 404;
+    protected const string url = "http://52.79.80.98:5000";
+    //protected const string url = "http://127.0.0.1:5000";
+
+    public enum HttpRequestType { GET, POST }
+
+    public delegate void ResponseCallback(long resCode, UnityWebRequest response);
+
+    public void PostJson(string jsonString, ResponseCallback callback)
+    {
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonString);
+        var request = new UnityWebRequest(url, "POST");
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        StartCoroutine(WaitRequest(request, callback));
+    }
+
+    public static IEnumerator WaitRequest(UnityWebRequest request, ResponseCallback callback)
+    {
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError)
+        {
+            Debug.LogError(request.error);
+        }
+
+        callback(request.responseCode, request);
+
+        request.Dispose();
+    }
+}
---
+++ Assets/Scripts/SGLogin.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using LitJson;
+
+public class SGLogin : MonoBehaviour {
+    public Text nicknameText;
+    public static string nickname = "";
+
+    public void OnclickLogin()
+    {
+        var httpClient = SGHttpClient.Instance;
+
+        ReqLogin req = new ReqLogin();
+        req.nickname = nicknameText.text;
+
+        string stringJson = JsonMapper.ToJson(req);
+
+        httpClient.PostJson(stringJson, ResponseCallback);
+    }
+
+    void ResponseCallback(long resCode, UnityWebRequest response)
+    {
+        string data = response.downloadHandler.text;
+        ResLogin res = JsonUtility.FromJson<ResLogin>(data);
+
+        if (res.resultCode == "2")
+        {
+            // 아이디 확인 팝업
+            //SceneManager.LoadScene("stage1");
+        }
+        else
+        {
+            nickname = res.nickname;
+            SceneManager.LoadScene("stage1");
+        }
+    }
+}
+
+[Serializable]
+public class ReqLogin
+{
+    public string protocol = "ReqLogin";
+    public string nickname = "";
+}
+
+[Serializable]
+public class ResLogin
+{
+    public string protocol;
+    public string resultCode;
+    public string message;
+    public string nickname;
+}
+
+
